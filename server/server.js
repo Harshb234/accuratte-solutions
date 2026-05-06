5e4338e889b7b2ef050bd19eeedd7027aab58b82
Harsh Bambatkar <harshbambatkar0502@gmail.com>
2026-05-06 23:09:28 +0530

    Explicitly configure CORS for production domains

--- server/server.js
+++ server/server.js
@@ -12,7 +12,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://accurattesolutions.in', 'https://www.accurattesolutions.in'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
